4ad27bb7220e9d7d2598e8db24a5d79f780fabda
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-14 23:41:58 +0200

    Update page.tsx

--- src/app/slider/page.tsx
+++ src/app/slider/page.tsx
@@ -2,55 +2,11 @@
 import Image from 'next/image';
 import { useState } from 'react';
 
-const Slider = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
-  };
-
+const Slider = () => {
+  
   return (
     <div className="relative w-full overflow-hidden flex justify-center items-center">
-      <div
-        className="flex transition-transform duration-500"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-      >
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className="w-full flex-shrink-0 p-4 bg-gray-900 rounded-lg flex flex-col items-center justify-center"
-          >
-            <Image
-              src={slide.image}
-              alt={slide.title}
-              width={500} // Adjust width and height as per your design
-              height={300}
-              className="w-full h-64 object-cover rounded-lg mb-4"
-            />
-            <h4 className="text-xl font-semibold mb-2 text-center text-white">{slide.title}</h4>
-            <p className="text-center text-white">{slide.content}</p>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Join Now
-            </button>
-          </div>
-        ))}
-      </div>
-      <button
-        onClick={prevSlide}
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2"
-      >
-        Prev
-      </button>
-      <button
-        onClick={nextSlide}
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2"
-      >
-        Next
-      </button>
+     
     </div>
   );
 };
